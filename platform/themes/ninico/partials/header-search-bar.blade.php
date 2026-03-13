e4e451239c3cdaa8d0cbd0ec9ce20a8234b33901
Jehanzeb Ali <jehanzebsherali@gmail.cmo>
2026-03-14 01:26:43 +0800

    no message

--- platform/themes/ninico/partials/header-search-bar.blade.php
+++ platform/themes/ninico/partials/header-search-bar.blade.php
@@ -1,26 +1,68 @@
-@if(is_plugin_active('ecommerce') && 1==false)
+@if(is_plugin_active('ecommerce'))
     <div class="header-search-bar">
-        <form action="{{ route('public.products') }}" class="position-relative form--quick-search" data-url="{{ route('public.ajax.search-products') }}" method="GET">
-            <div class="search-info p-relative">
-                @if($hasCategorySelect = (theme_option('display_product_categories_select_on_header', 'yes') === 'yes'))
-                    <div class="product-category-label">
-                        <label class="form-label" for="product-category-select" class="text text-truncate d-inline-block">{{ __('All Categories') }}</label>
-                        <i class="fal fa-chevron-down ms-2"></i>
-                    </div>
-                    <select
-                        class="product-category-select"
-                        name="categories[]"
-                    >
-                        <option value="0">{{ __('All Categories') }}</option>
-                        {!! ProductCategoryHelper::renderProductCategoriesSelect() !!}
-                    </select>
-                @endif
-                <input type="text" name="q" @class(['input-search-product', 'has-category-select' => $hasCategorySelect]) placeholder="{{ __('Search products...') }}" value="{{ BaseHelper::stringify(request()->query('q')) }}" autocomplete="off">
-                <button class="header-search-icon" title="search">
-                    <i class="fal fa-search"></i>
-                </button>
+        <button class="btn p-0 border-0 bg-transparent"
+                data-bs-toggle="modal"
+                data-bs-target="#searchModal"
+                title="search">
+            <i class="fal fa-search"></i>
+        </button>
+    </div>
+    <div class="modal fade" id="searchModal" tabindex="-1" aria-hidden="true">
+        <div class="modal-dialog modal-lg modal-dialog-centered">
+            <div class="modal-content p-4">
+
+                <div class="modal-header border-0">
+                    <h5 class="modal-title">{{ __('Search Products') }}</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+                </div>
+
+                <div class="modal-body">
+
+                    <form action="{{ route('public.products') }}"
+                          class="position-relative form--quick-search"
+                          data-url="{{ route('public.ajax.search-products') }}"
+                          method="GET">
+
+                        <div class="search-info p-relative">
+
+                            @if($hasCategorySelect = (theme_option('display_product_categories_select_on_header', 'yes') === 'yes'))
+                                <div class="product-category-label mb-2">
+                                    <label class="form-label text-truncate d-inline-block">
+                                        {{ __('All Categories') }}
+                                    </label>
+                                    <i class="fal fa-chevron-down ms-2"></i>
+                                </div>
+
+                                <select class="form-select mb-3 product-category-select"
+                                        name="categories[]">
+                                    <option value="0">{{ __('All Categories') }}</option>
+                                    {!! ProductCategoryHelper::renderProductCategoriesSelect() !!}
+                                </select>
+                            @endif
+
+                            <div class="input-group">
+
+                                <input type="text"
+                                       name="q"
+                                       @class([
+                                           'form-control input-search-product',
+                                           'has-category-select' => $hasCategorySelect
+                                       ])
+                                       placeholder="{{ __('Search products...') }}"
+                                       value="{{ BaseHelper::stringify(request()->query('q')) }}"
+                                       autocomplete="off">
+
+                                <button class="btn btn-dark" title="search">
+                                    <i class="fal fa-search"></i>
+                                </button>
+
+                            </div>
+
+                        </div>
+                        <div class="panel--search-result"></div>
+                    </form>
+                </div>
             </div>
-            <div class="panel--search-result"></div>
-        </form>
+        </div>
     </div>
 @endif
